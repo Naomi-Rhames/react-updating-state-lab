81d811ad7f5915f68732b9d2c88b86e860e20110
Naomi-Rhames <naomirhames2020@gmail.com>
2021-10-12 19:13:52 -0500

    commit tree 4aad069acac29c60d9cca2bf8b385c71cbff0ffb

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,44 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component{
+
+    constructor() {
+        super();
+    
+        this.state = {
+          timesClicked: 0,
+        };
+      }
+    
+      handleClick = () => {
+        this.setState(prevState => ({
+          timesClicked: prevState.timesClicked+1
+        }))
+      }
+    
+      render() {
+        return (
+          <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        );
+      }
+    }
+//   constructor(){
+//       super()
+
+//       this.state = {
+//           clicked: 0
+//       }
+//   }
+
+//   handleClicked = () => {
+//       this.setState(prevState => ({
+//         clicked: prevState.clicked+1
+//       }))
+//   }
+
+//   render() {
+//       return(
+//           <button onClick={this.handleClicked}>{this.state.clicked}</button>
+//       )
+//   }
+// }
